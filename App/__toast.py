86dfcbfd400b47f4ffbfed66e74c4f5ef7391b35
wbbb <jus4hr@outlook.com>
2024-01-01 21:19:37 +0800

    fix toaster

--- App/__toast.py
+++ App/__toast.py
@@ -1,13 +1,13 @@
-from windows_toasts import ToastDuration, ToastText1, WindowsToaster
+from windows_toasts import ToastDuration, Toast, WindowsToaster
 
 from . import __utils as _u
 
 
 def toast(message: str, long: bool = False):
-    _text.SetBody(message)
-    _text.SetDuration(ToastDuration.Long if long else ToastDuration.Short)
+    _text.text_fields.append(message)
+    _text.duration =(ToastDuration.Long if long else ToastDuration.Short)
     _toaster.show_toast(_text)
 
 
 _toaster = WindowsToaster(_u.APP_NAME)
-_text = ToastText1()
+_text = Toast()
